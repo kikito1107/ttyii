64740fd16dd54ee34c486b921376f09af3fca25b
javierlimp <deftoner.javier@gmail.com>
2017-03-02 15:37:37 -0600

    repertorio

--- config/db.php
+++ config/db.php
@@ -1,24 +1,24 @@
 <?php
 
 // Martinez
-/*return [
+return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost; dbname=tt',
     'username' => 'root',
-    'password' => '',
+    'password' => '123456',
     'charset' => 'utf8',
     'tablePrefix' => 'tbl_'
-];*/
+];
 
 
 // Saul
-return [
+/*return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost; dbname=ttyii',
     'username' => 'tt',
     'password' => '123456',
     'charset' => 'utf8',
     'tablePrefix' => 'tbl'
-];
+];*/
 
 ?>
